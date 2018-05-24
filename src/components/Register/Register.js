19b3756385422a341278984911d4da58c22fffc6
GUDevs <groundup365@gmail.com>
2018-05-24 00:09:33 -0400

    Connected Frontend to Backend

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://damp-falls-27810.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
